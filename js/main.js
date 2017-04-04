a35424f804bc0f4c48fbdab2ed77e770daa0cbe2
Julia Kim <jkim0620@gmail.com>
2017-04-04 17:02:40 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
-var cardOne = "queen";
-var caedTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+// var cardOne = "queen";
+// var caedTwo = "king";
+// var cardThree = "queen";
+// var cardFour = "king";
+
+// console.log("User flipped " + cardOne);
+// console.log("User flipped " + cardTwo);
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+
+cardsInPlay.push(cardTwo);
 
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
