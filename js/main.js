39437f6aade84939f544616b67fdc47341041a9a
Julia Kim <jkim0620@gmail.com>
2017-04-04 23:40:26 -0400

    created funtions

--- js/main.js
+++ js/main.js
@@ -1,27 +1,23 @@
-// var cardOne = "queen";
-// var caedTwo = "king";
-// var cardThree = "queen";
-// var cardFour = "king";
-
-// console.log("User flipped " + cardOne);
-// console.log("User flipped " + cardTwo);
-
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
 
-cardsInPlay.push(cardOne);
+var flipCard = function (cardId) {
+	
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
 
-cardsInPlay.push(cardTwo);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}	
+}
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
+var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
 		alert("Sorry, try again.");
 	}
 }
+
+flipCard(3);
+flipCard(2);
